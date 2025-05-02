4e59aabc2b3226928798308e224da30e23800058
smitexq <spygidak@gmail.com>
2025-05-02 16:48:27 +0300

    - в RegistrationService добавлена проверка на то, что пользователь ввел данные во всех 3 поля. Иначе повторая регистрация - изменение сообщения при редиректе на /login - в шаблонах к Login page и Add new user соответственно добавлены теги <div>

--- src/main/java/com/cweater/cweater/controllers/RegistrationController.java
+++ src/main/java/com/cweater/cweater/controllers/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -23,7 +24,9 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(
+            User user,
+            Map<String, Object> model) {
         return service.addUser(user, model);
     }
 
@@ -31,4 +34,15 @@ public class RegistrationController {
     public String activate(Map<String, Object> model, @PathVariable String code) {
         return service.activate(model, code);
     }
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+//
+//    @PostMapping("/login")
+//    public String authentication(User user, Map<String, Object> model) {
+//        return service.addUser(user, model);
+//    }
+
 }
--- src/main/java/com/cweater/cweater/service/RegistrationService.java
+++ src/main/java/com/cweater/cweater/service/RegistrationService.java
@@ -3,7 +3,9 @@ package com.cweater.cweater.service;
 import com.cweater.cweater.entities.Role;
 import com.cweater.cweater.entities.User;
 import com.cweater.cweater.repository.UserRepo;
+import com.cweater.cweater.sheduled.DeleteInactiveUser;
 import com.cweater.cweater.tools.Urls;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import java.util.UUID;
 
 @Service
 public class RegistrationService {
+    @Autowired
+    private DeleteInactiveUser deleteInactiveUser;
 
     private final UserRepo userRepo;
     private final MailSender mailSender;
@@ -23,19 +27,29 @@ public class RegistrationService {
     }
 
     public String addUser(User user, Map<String, Object> model) {
+        if (user.getEmail().isEmpty() || user.getUsername().isEmpty() || user.getPassword().isEmpty()) {
+            model.put("message","Заполните все поля!");
+            return "registration";
+        };
+
+
         Optional<User> userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb.isPresent()) {
+        if (userFromDb.isPresent()) { //Если пользователь уже есть
             model.put("message","User exist!");
             return "registration";
         }
-        user.setActive(true);
+
+        user.setActive(false);
         user.setRoles(Collections.singleton(Role.USER));
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         //Код активации аккаунта
         user.setActivationCode(UUID.randomUUID().toString());
 
         userRepo.save(user);
+        //Через 60 секунд удаляем пользователя из БД, если он не активировал аккаунт
+        deleteInactiveUser.sheduledDeleteUser(user.getId());
 
+        //Отправка сообщения на почту
         StringBuilder msg = new StringBuilder();
         msg.append("Hello, ")
                 .append(user.getUsername())
@@ -48,15 +62,17 @@ public class RegistrationService {
                 "Activate your account",
                 String.valueOf(msg));
 
+        model.put("message","Проверь свою почту и перейди по ссылке для активации аккаунта!");
         return "redirect:/login";
     }
 
     public String activate(Map<String, Object> model, String code) {
         User user = userRepo.findByActivationCode(code);
 
-        if (user == null) {model.put("message", "Неверный код активации!");}
+        if (user == null) {model.put("message", "Срок действия ссылки истек или неверный код активации!");}
         else {
             user.setActivationCode(null);
+            user.setActive(true);
             userRepo.save(user);
             model.put("message", String.format("Аккаунт пользователя %s активирован. Войдите в систему.", user.getUsername()));
         }
